8c846aa95a6757b36875ed99433dcd3956921661
orion_pt <info@orioneurocorp.com>
2026-08-25 16:32:37 +0100

    Major refactor - initial commit

---
+++ resources/dist/vendor/genie-enterprise/assets/usePageMode-C7txFMlS.js
@@ -0,0 +1,1 @@
+import{A as e,cA as s}from"./app-D-eAv1Fp.js";const a=()=>{const t=e(()=>s().props.mode),r=e(()=>t.value==="create"),o=e(()=>t.value==="edit");return{isCreate:r,isEdit:o}};export{a as u};
